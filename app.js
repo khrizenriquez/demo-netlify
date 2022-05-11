efae303294d6e1e361dfe777440ad961e4d8a54b
khrizenriquez <christoferen7@gmail.com>
2022-05-10 23:51:38 -0600

    Basic data is being loaded

--- app.js
+++ app.js
@@ -1,30 +1,60 @@
 'use strict'
 
-const loadMovies = async () => {
-    try {
-        const response = await fetch('https://api.themoviedb.org/3/movie/popular?api_key=42431c3f9638f4b0885de2c95c89be4c')
+//  -------------------- Variable definitions
+//  width 500
+const imagePath = 'https://image.tmdb.org/t/p/w500/'
 
-        console.log(response)
-        switch (response.status) {
-            case 200:
-                const data = await response.json()
-                //console.log(data.results)
-                data.results.forEach(item => {
-                    console.log(item.title)
-                });
-                break
-            case 401:
-                console.error(response)
-                break
-            case 404:
-                console.error(response)
-                break
-            default:
-                console.log(response)
-                break
-        }
-    } catch (error) {
-        console.log(error)
+//  -------------------- Functions definitions
+const loadMovieS = () => {
+    const fetchMovie = async () => {
+        const response = await fetch('https://api.themoviedb.org/3/movie/popular?api_key=42431c3f9638f4b0885de2c95c89be4c')
+        
+        const movies = await response.json()
+        return movies
     }
+    fetchMovie().then(movies => {
+        return fillMovieSection(movies.results)
+    })
+    fetchMovie().catch(error => console.log(error))
 }
-loadMovies()
+const fillMovieSection = (movieData) => {
+    console.log(movieData)
+    const itemContainer = document.querySelector('#main-container')
+
+    //  Cleaning up the main container
+    itemContainer.innerHTML = ''
+
+    let itemImage = '', 
+        itemTitle = '',
+        itemAvg = 0, 
+        itemTotalVotes = 0
+
+    function movieImage (imageSrc, imageAlt) {
+        return  `<div class='image-container'>
+                    <img src="${ imagePath + imageSrc }" alt="${ imageAlt }" title="${ imageAlt }" />
+                </div>`
+    }
+    function movieTitle (imageSrc, imageAlt) {
+        return  `<div class='image-container'>
+                    <img src="${ imagePath + imageSrc }" alt="${ imageAlt }" title="${ imageAlt }" />
+                </div>`
+    }
+
+    let rawContent = ''
+    movieData.forEach((element, index) => {
+        console.log(element)
+        rawContent +=   `<div class="list-item">
+                            ${ movieImage(element.poster_path, element.title) }
+                            <div class='items-container'>
+                                <h5>Vote avg / Total vote</h5>
+                                <h3>${ element.title }</h3>
+                            </div>
+                        </div>`
+    })
+
+    //  Filling the main container with the movies items
+    itemContainer.innerHTML = rawContent
+}
+
+//  -------------------- Init
+loadMovieS()
